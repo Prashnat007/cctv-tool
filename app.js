4a73df99500fc413cc066468dbb21cfc9c6f9426
Prashnat007 <prashantmaurya600@gmail.com>
2025-01-20 12:29:20 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const { useState } = React;
+const { Camera, Home, Building2, Cloud, Sun, ExternalLink, ShoppingCart, Eye, Shield, Wifi, Moon } = lucide;
+
+function CCTVRecommender() {
+    // [Previous React component code from earlier goes here]
+    // Copy the entire component code we created earlier
+}
+
+// Render the app
+ReactDOM.render(<CCTVRecommender />, document.getElementById('root'));
